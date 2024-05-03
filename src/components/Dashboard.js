6cf2713a6e51d87023c834a3b690f16e53548852
Manuel Perez <mperez@wawand.co>
2024-05-03 11:01:57 -0500

    updating payments page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -41,7 +41,7 @@ export default function Dashboard({ activeTab, children }) {
                             </a>
                         </li> */}
                         <li className='m-0'>
-                            <a href="/payments" className={(activeTab === "payments" ? "bg-gray-300 dark:bg-[#7091E6] " : "") + "flex items-center p-2 text-gray-900 dark:text-white hover:bg-gray-300 dark:hover:bg-[#7091E6] group"}>
+                            <a href="/" className={(activeTab === "payments" ? "bg-gray-300 dark:bg-[#7091E6] " : "") + "flex items-center p-2 text-gray-900 dark:text-white hover:bg-gray-300 dark:hover:bg-[#7091E6] group"}>
                                 <svg className={"flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-white"} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
                                     <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
                                 </svg>
